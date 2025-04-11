c86eab83dfa4e66266fc66f0aa88769037f7013e
kanarus <kanarus786@gmail.com>
2025-04-12 03:12:49 +0900

    Bode Gain Plot and Nyquist Plot

--- FlexibleLink/analysis/cmd/graphviz.py
+++ FlexibleLink/analysis/cmd/graphviz.py
@@ -24,7 +24,7 @@ class AnalysisTarget:
         with open(filename, mode='r') as f:
             self.__dataframe = DataFrame(
                 list(csv.reader(f)),
-                index=['ω', 'G', 'SysPhase']
+                index=['ω', 'SysGain', 'SysPhase']
             )
 
     def __str__(self):
@@ -33,8 +33,40 @@ class AnalysisTarget:
     def ω(self) -> list[float]:
         return list(map(float, self.__dataframe.loc['ω'].to_list()))
     
-    def G(self) -> list[float]:
-        return list(map(float, self.__dataframe.loc['G'].to_list()))
+    def SysGain(self) -> list[float]:
+        return list(map(float, self.__dataframe.loc['SysGain'].to_list()))
+    
+    def SysPhase(self) -> list[float]:
+        return list(map(float, self.__dataframe.loc['SysPhase'].to_list()))
+    
+    def saveBodeGainPlot(self, filename: str) -> None:
+        figure = pyplot.figure()
+        pyplot.title('Bode Gain Plot')
+        pyplot.xscale('log')
+        pyplot.xlabel('ω [rad/sec]')
+        pyplot.ylabel('20log|G(jω)|')
+        pyplot.grid()
+        pyplot.scatter(
+            x=self.ω(),
+            y=list(map(lambda x: 20 * math.log10(x), self.SysGain())),
+            s=8,
+        )
+        with open(filename, mode='w') as f:
+            figure.savefig(f, format='svg')
+
+    def saveNyquistPlot(self, filename: str) -> None:
+        figure = pyplot.figure()
+        pyplot.title('Nyquist Plot')
+        pyplot.xlabel('Re(G(jω))')
+        pyplot.ylabel('Im(G(jω))')
+        pyplot.grid()
+        pyplot.scatter(
+            x=list(map(lambda x, y: x * math.cos(y), self.SysGain(), self.SysPhase())),
+            y=list(map(lambda x, y: x * math.sin(y), self.SysGain(), self.SysPhase())),
+            s=8,
+        )
+        with open(filename, mode='w') as f:
+            figure.savefig(f, format='svg')
 
 
 def main():
@@ -44,26 +76,18 @@ def main():
     print(data)
     print('ω =', data.ω(), type(data.ω()))
 
-    figure = pyplot.figure()
-    pyplot.title('Bode Gain plot')
-    pyplot.xlabel('ω')
-    pyplot.ylabel('20log|G(jω)|')
-    pyplot.scatter(
-        data.ω(),
-        list(map(lambda x: 20 * math.log10(x), data.G())),
-    )
-    save_file_path = path.join(
+    data.saveBodeGainPlot(path.join(
         path.dirname(__file__),
         '..',
         'graph',
-        f'{path.splitext(path.basename(a.filename))[0]}.graph.svg'
-    )
-    with open(save_file_path, mode='w') as f:
-        figure.savefig(
-            f,
-            format='svg',
-        )
-
+        f'{path.splitext(path.basename(a.filename))[0]}.BodeGainPlot.svg'
+    ))
+    data.saveNyquistPlot(path.join(
+        path.dirname(__file__),
+        '..',
+        'graph',
+        f'{path.splitext(path.basename(a.filename))[0]}.NyquistPlot.svg'
+    ))
 
 if __name__ == "__main__":
     main()
