7fad7485e2fff209dc314cd6bc5ac5a2a1946cb6
kanarus <kanarus786@gmail.com>
2025-04-13 03:19:03 +0900

    refactor SampleData.__init__

--- FlexibleLink/analysis/src/lib/data.py
+++ FlexibleLink/analysis/src/lib/data.py
@@ -10,6 +10,22 @@ class Plot:
     y: list[float]
 
 class SampleData:
+    """
+    Parsed data from a CSV file containing frequency response data.
+    The CSV file should have three float rows:
+
+    1. ω (angular frequency)
+    2. System Gain (SysGain)
+    3. System Phase (SysPhase)
+
+    sorted by columns in ascending order of ω.
+
+    Methods:
+
+    - `ω`, `SysGain`, `SysPhase`: view the data in the respective rows.
+    - `BodeGainPlot`, `NyquistPlot`: generate plot data for Bode Gain or Nyquist plots.
+    """
+
     __dataframe: pandas.DataFrame
 
     def __init__(self, filename: str):
@@ -18,19 +34,22 @@ class SampleData:
                 csv.reader(f),
                 dtype=float,
                 index=['ω', 'SysGain', 'SysPhase'],
-            )
+            ).T.sort_values(
+                by='ω',
+                ascending=True,
+            ) # .T: transpose for pandas's column-oriented data structure
 
     def __str__(self):
         return str(self.__dataframe)
     
     def ω(self) -> list[float]:
-        return self.__dataframe.loc['ω'].to_list()
+        return self.__dataframe['ω'].to_list()
     
     def SysGain(self) -> list[float]:
-        return self.__dataframe.loc['SysGain'].to_list()
+        return self.__dataframe['SysGain'].to_list()
     
     def SysPhase(self) -> list[float]:
-        return self.__dataframe.loc['SysPhase'].to_list()
+        return self.__dataframe['SysPhase'].to_list()
     
     def BodeGainPlot(self) -> Plot:
         return Plot(
@@ -44,6 +63,6 @@ class SampleData:
             y=list(map(lambda x, y: x * math.sin(y), self.SysGain(), self.SysPhase())),
         )
     
-    # def eliminate(self, )
+    # def remove_columns(self
     
 
